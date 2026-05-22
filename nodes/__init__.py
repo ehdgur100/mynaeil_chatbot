728186ccfbe91c6c5d505ea683dfe4c786aca0d6
lyk-20 <prhgbbd@gmail.com>
2026-05-22 16:29:10 +0900

    파일 경로 수정
    파일 경로 변경 관련 코드 수정

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -1,6 +1,6 @@
+from nodes.onboarding import resume_gen
 from nodes.intent import analyze_intent
 from nodes.policy import policy_search
-from nodes.resume import resume_gen
 from nodes.resume_verify import resume_verify
 from nodes.job import job_search
 from nodes.education import edu_recommend
@@ -8,9 +8,9 @@ from nodes.guide import apply_guide
 from nodes.basic import basic_chat
 
 __all__ = [
+    "resume_gen",
     "analyze_intent",
     "policy_search",
-    "resume_gen",
     "resume_verify",
     "job_search",
     "edu_recommend",
